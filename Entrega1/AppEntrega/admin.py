4eb0ef3c718b575fa1f954762245438bf0c3717d
ElProleOP <103580268+ElProleOP@users.noreply.github.com>
2022-04-13 13:15:55 -0300

    Delete Entrega1/AppEntrega directory

--- Entrega1/AppEntrega/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from AppEntrega.models import Libros, Socio, Juegomesa
-
-# Register your models here.
-
-admin.site.register(Libros)
-admin.site.register(Socio)
-admin.site.register(Juegomesa)
--- Entrega1/AppEntrega/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AppentregaConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'AppEntrega'
